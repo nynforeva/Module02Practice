f3c60d8336c857d5298fb8ac8b434bdd94011a4d
Dilnaz <Dilnaz@ULTRAMACHINE>
2026-09-17 15:53:25 +0500

    Добавьте файлы проекта.

---
+++ Module02Practice/Controllers/StudentsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Module02Practice.Models;
+
+namespace Module02Practice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private static readonly List<Student> students = new()
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "Alex",
+                Group = "SE-301"
+            },
+
+            new Student
+            {
+                Id = 2,
+                Name = "Anna",
+                Group = "SE-302"
+            },
+
+            new Student
+            {
+                Id = 3,
+                Name = "Max",
+                Group = "SE-301"
+            }
+        };
+
+        // GET: api/students
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAll()
+        {
+            return Ok(students);
+        }
+
+        // GET: api/students/2
+        [HttpGet("{id:int}")]
+        public ActionResult<Student> GetById(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // POST: api/students
+        [HttpPost]
+        public ActionResult<Student> Create(Student student)
+        {
+            students.Add(student);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = student.Id },
+                student
+            );
+        }
+    }
+}
---
+++ Module02Practice/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Module02Practice.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Group { get; set; } = string.Empty;
+    }
+}
